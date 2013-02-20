b8a5bc573bfd084f7ce40caf0b1d528d4f812348
Charles Miglietti <charles.miglietti@yahoo.fr>
2013-02-20 18:54:24 +0100

    schedulere ok

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-var mqClient = require('../lib/message-queue') ;
+var RedisQueue = require('node-redis-pubsub')
+  , env = process.env.NODE_ENV || 'development'
+  , options = { scope: env, port: 6379 }
+  , mqClient = new RedisQueue(options);
+
+
+mqClient.on('schedule', function ( data ) {
+  var delay = data.delay
+    , event = data.event
+    , toSend = data.data;
+
+    console.log('Receiving', event, delay, toSend);
+  setTimeout(function() {
+    mqClient.emit(event, toSend );
+    console.log('Sending', event, delay, toSend);
+  }, delay * 1000);
+});
